3da8cbc360563c9ee6e63099ffbe5f26e947beed
ilya <imakovoz93@gmail.com>
2018-06-01 15:40:25 -0400

    init commit

---
+++ js/map.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  var map;
+  function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      zoom: 8
+    });
+  }
+});
