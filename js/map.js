f09087eeb1c52a2e60a4ca35ea9227b7439e01f4
ilya <imakovoz93@gmail.com>
2018-06-01 17:48:57 -0400

    change css to grid

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
+  console.log('test');
   var map;
   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
