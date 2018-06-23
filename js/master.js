484bb51c541296fb877ffdf03125b8d5a0d069aa
ilya <imakovoz93@gmail.com>
2018-06-23 16:28:25 -0400

    facebook css hidden

--- js/master.js
+++ js/master.js
@@ -6,20 +6,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
     // adaptiveHeight: true
   });
 
-  var test = window.matchMedia("(max-width: 700px)");
-  if (test.matches) {
-    document.getElementById("facbookprof").setAttribute("data-height", "200");
-  }
-  (function(d, s, id) {
-    var js,
-      fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src =
-      "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=223747385023298&autoLogAppEvents=1";
-    fjs.parentNode.insertBefore(js, fjs);
-  })(document, "script", "facebook-jssdk");
   var links = ["inicio", "procedimientos", "trajectoria", "contacto"];
   var elem = null;
   links.forEach(e => {
