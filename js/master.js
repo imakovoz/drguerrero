17acac7f66499e07e0bb033c832960580e88858c
ilya <imakovoz93@gmail.com>
2018-06-23 16:18:19 -0400

    move script

--- js/master.js
+++ js/master.js
@@ -10,7 +10,16 @@ document.addEventListener("DOMContentLoaded", function(event) {
   if (test.matches) {
     document.getElementById("facbookprof").setAttribute("data-height", "200");
   }
-
+  (function(d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src =
+      "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0&appId=223747385023298&autoLogAppEvents=1";
+    fjs.parentNode.insertBefore(js, fjs);
+  })(document, "script", "facebook-jssdk");
   var links = ["inicio", "procedimientos", "trajectoria", "contacto"];
   var elem = null;
   links.forEach(e => {
