39ad7c1735c00185ebda7cfdec6e085c498be2ed
ilya <imakovoz93@gmail.com>
2018-06-23 15:44:42 -0400

    add javascript scroll

--- js/master.js
+++ js/master.js
@@ -1,8 +1,20 @@
 document.addEventListener("DOMContentLoaded", function(event) {
   $(".single-item").slick({
-  	dots: true,
+    dots: true,
     autoplay: true,
-    infinite: true,
+    infinite: true
     // adaptiveHeight: true
+  });
+  var links = ["inicio", "procedimientos", "trajectoria", "contacto"];
+  var elem = null;
+  links.forEach(e => {
+    document.getElementById(e + "link").addEventListener("click", e => {
+      elem = "#" + e.target.id.slice(0, -4);
+      $("html, body").animate({ scrollTop: $(elem).offset().top - 90 }, "slow");
+      console.log(elem);
+      // document.getElementById(elem).scrollIntoView({
+      //   behavior: "smooth"
+      // });
+    });
   });
 });
